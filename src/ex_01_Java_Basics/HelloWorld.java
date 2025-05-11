863952d3fe8352979497bcf84484b777034ba92e
venkatesh yadav <venkateshyadav0553@gmail.com>
2025-05-11 17:34:34 +0530

    Initial commit

---
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class HelloWorld {
+
+public static void main(String[] args){
+
+    System.out.println("Hello venkatesh you are going something special");
+
+  }
+}
