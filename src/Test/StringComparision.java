d493556c2e6414e3f4ab92e369e30f327a4e03a4
venkatesh yadav <venkateshyadav0553@gmail.com>
2025-06-28 12:03:23 +0530

    String programs

---
+++ src/Test/StringComparision.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class StringComparision {
+    public static void main(String[] args){
+//        Create a program that demonstrates different ways to compare strings in Java,
+//        including == operator, equals(), equalsIgnoreCase(), and compareTo() methods.
+           String s1 = "Hello";
+           String s2 = "hello";
+           String s3 = "Hello";
+
+
+    }
+}
